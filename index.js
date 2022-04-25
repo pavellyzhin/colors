c7dd9416fc86c78fa359ea3b50eab282699fc6a1
pavellyzhin <pavellyzhin@yandex.ru>
2022-04-25 15:19:41 +0300

    refactor: конечная версия для тестовой демо

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const container = document.createElement('div');
 app.className = 'app';
 app.textContent = 'Текст';
 
-try{
-
+try {
     container.style.color = (new Red()).getColor();
+
     container.textContent = 'текст2';
+
     app.appendChild(container);
+
     app.style.color = (new Orange()).getColor();
 
     document.querySelector('body').appendChild(app);
     
 }catch(error){
+
     console.log(error.stack);
+    
 }
 
 
